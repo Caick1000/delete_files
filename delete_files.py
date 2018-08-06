aad7530f5358a3bdbb8d544f18d44c68eea02b5e
DESKTOP-U4G76IE\Caick <caick.passarella@talkeen.com.br>
2018-08-06 17:35:03 -0300

    added a function

--- delete_files.py
+++ delete_files.py
@@ -1,30 +1,30 @@
 from os import listdir, unlink, path
-from os.path import getctime
+from os.path import getctime, isfile
 from time import time
+
 
 dir_path = 'var/log/'
 dir_path_extension = ['asterisk', 'contact']
-removable_file_extensions = ['.log']
+removable_file_extensions = ['log']
 
 current_time = time()
-
-for f in os.listdir(dir_path):
-    creation_time = os.path.getctime(f)
-    if (current_time - creation_time) // (24 * 3600) >= 7:
-        os.unlink(f)
-        print('{} removed from directory {}'.format(f, dir_path))
+minimum_date = 365
 
 for i in dir_path_extension:
     index_value = i
-    full_path = dir_path + index_value[i]
+    full_path = dir_path + index_value
 
 
 
 def delete_files(dir_path):
-    for file in listdir(dir_path):
-        file_date = getctime(file)
 
-        if (current_time - file_date) // (24 * 3600) >= 365 and removable_file_extensions in file[:-3]:
-            print(file)
+    for file in listdir(dir_path):
+        file_date = getctime(dir_path + file)
+        # if (current_time - file_date) // (24 * 3600) >= minimum_date and removable_file_extensions in file[-3:] and isfile(dir_path):
+        if isfile(dir_path):
+            print(file, file[:-3])
             # os.unlink(file)
-        print('"{}" removed from directory "{}"'.format(file, dir_path))
+            print('"{}" removed from directory "{}"'.format(file, dir_path))
+
+    
+delete_files('D:/talkeen/test/')
