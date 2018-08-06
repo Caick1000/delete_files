62444ffefb394e570d62d2475da85e7ebb9312e7
DESKTOP-U4G76IE\Caick <caick.passarella@talkeen.com.br>
2018-08-06 17:38:41 -0300

    made it work (lol)

--- delete_files.py
+++ delete_files.py
@@ -21,10 +21,9 @@ def delete_files(dir_path):
     for file in listdir(dir_path):
         file_date = getctime(dir_path + file)
         # if (current_time - file_date) // (24 * 3600) >= minimum_date and removable_file_extensions in file[-3:] and isfile(dir_path):
-        if isfile(dir_path):
-            print(file, file[:-3])
+        if isfile(dir_path + file) and file[-3:] in removable_file_extensions:
             # os.unlink(file)
             print('"{}" removed from directory "{}"'.format(file, dir_path))
 
     
-delete_files('D:/talkeen/test/')
+delete_files('D:/talkeen/')
